401b9f0f77452da52ec43ba192c41a9555cd9dcd
Lenon Corrêa <lenonac_@hotmail.com>
2025-04-27 07:49:16 -0300

    fix: corrige erro em build de produção da rota login para /

--- frontend/app/API/config.js
+++ frontend/app/API/config.js
@@ -1,4 +1,4 @@
 import Cookies from 'js-cookie';
 
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3002/api/v1";
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/api/v1";
 export const  TOKEN = Cookies.get("auth_token") || null;
