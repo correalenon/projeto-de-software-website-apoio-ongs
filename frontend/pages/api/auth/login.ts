489fe43e546c53032168902dd589d7f6668c096f
Lenon Antonio Corrêa <lenon.correa@stoque.com.br>
2025-04-30 01:08:57 -0300

    fix: Valida função

--- frontend/pages/api/auth/login.ts
+++ frontend/pages/api/auth/login.ts
@@ -3,6 +3,7 @@ import { serialize } from "cookie";
 import { API_URL } from "../../../app/API/config"
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  return res.status(200).json({ message: "Login API" });
   if (req.method !== "POST") {
     return res.status(405).json({ message: "Method Not Allowed" });
   }
