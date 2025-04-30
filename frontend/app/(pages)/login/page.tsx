905442bb97c983182970ac8363aae034286ed0e6
Lenon Antonio Corrêa <lenon.correa@stoque.com.br>
2025-04-30 01:01:55 -0300

    fix: Valida URL

--- frontend/app/(pages)/login/page.tsx
+++ frontend/app/(pages)/login/page.tsx
@@ -22,7 +22,7 @@ export default function LoginPage() {
       return
     }
     try {
-      const response = await fetch("http://localhost:3001/api/auth/login", {
+      const response = await fetch("/api/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password })
