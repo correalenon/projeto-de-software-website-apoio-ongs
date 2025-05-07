bdac4c26ff6a2c831b6a0ee10f98b75e82cf7803
Lenon Antonio Corrêa <lenon.correa@stoque.com.br>
2025-05-07 09:22:09 -0300

    fix: Testa middleware

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,31 +1,47 @@
-import { NextResponse } from "next/server"
-import type { NextRequest } from "next/server"
+import { NextRequest, NextResponse } from 'next/server';
+
+// export function middleware(request: NextRequest) {
+//   const token = request.cookies.get("token")?.value;
+//   console.log("Token:", token);
+
+//   // Verificar se o usuário está acessando a página principal ou outras páginas protegidas
+//   const isProtectedRoute =
+//     request.nextUrl.pathname === "/" ||
+//     request.nextUrl.pathname === "/ongs" ||
+//     request.nextUrl.pathname === "/projects"
+
+//   // Verificar se o usuário está acessando páginas de autenticação
+//   const isAuthRoute = request.nextUrl.pathname === "/login" || request.nextUrl.pathname === "/signup"
+
+//   // Se for uma rota protegida e não tiver token, redirecionar para login
+//   if (isProtectedRoute && !token) {
+//     return NextResponse.redirect(new URL("/login", request.url))
+//   }
+
+//   // Se for uma rota de autenticação e já tiver token, redirecionar para home
+//   if (isAuthRoute && token) {
+//     return NextResponse.redirect(new URL("/", request.url))
+//   }
+
+//   return NextResponse.next()
+// }
 
 export function middleware(request: NextRequest) {
-  // Obter o token do cookie
-  const token = request.cookies.get("token")?.value;
+  const token = request.cookies.get('token')?.value;
   console.log("Token:", token);
 
-  // Verificar se o usuário está acessando a página principal ou outras páginas protegidas
-  const isProtectedRoute =
-    request.nextUrl.pathname === "/" ||
-    request.nextUrl.pathname === "/ongs" ||
-    request.nextUrl.pathname === "/projects"
+  const isAuth = !!token;
+  const isLoginPage = request.nextUrl.pathname === '/login';
 
-  // Verificar se o usuário está acessando páginas de autenticação
-  const isAuthRoute = request.nextUrl.pathname === "/login" || request.nextUrl.pathname === "/signup"
-
-  // Se for uma rota protegida e não tiver token, redirecionar para login
-  if (isProtectedRoute && !token) {
-    return NextResponse.redirect(new URL("/login", request.url))
+  if (!isAuth && !isLoginPage) {
+    return NextResponse.redirect(new URL('/login', request.url));
   }
 
-  // Se for uma rota de autenticação e já tiver token, redirecionar para home
-  if (isAuthRoute && token) {
-    return NextResponse.redirect(new URL("/", request.url))
+  if (isAuth && isLoginPage) {
+    return NextResponse.redirect(new URL('/dashboard', request.url));
   }
 
-  return NextResponse.next()
+  return NextResponse.next();
 }
 
 // Configurar em quais caminhos o middleware será executado
