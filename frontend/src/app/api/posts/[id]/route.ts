2326aa2f74d5ebceadd5bc7490b6724943da70fc
Lenon Antonio Corrêa <lenon.correa@stoque.com.br>
2025-05-08 16:28:43 -0300

    fix: Ajusta api interna com todos endpoints, já removendo anteriores não mais utilizados

--- frontend/src/app/api/posts/[id]/route.ts
+++ frontend/src/app/api/posts/[id]/route.ts
@@ -1,42 +1,46 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { API_URL } from "@/api/config";
 
-export async function GET(request: NextRequest) {
+export async function GET(request: NextRequest, context: { params: { id: number } }) {
     try {
         const TOKEN = request.cookies.get('token')?.value;
-        const response = await fetch(API_URL + "/posts", {
+        const params = await context.params;
+        const { id } = params;
+        const response = await fetch(API_URL + "/posts/" + id, {
             method: "GET",
             headers: {
                 "Content-Type": "application/json",
                 "Authorization": "Bearer " + TOKEN
             }
         });
-
+        
         if (!response.ok) {
-          return NextResponse.json({ error: 'Erro ao buscar posts' }, { status: response.status });
+          return NextResponse.json({ error: await response.json() }, { status: response.status });
         }
+
         return NextResponse.json(await response.json(), { status: response.status });
     } catch (error) {
         throw error;
     }
 }
 
-export async function POST(request: NextRequest) {
+export async function DELETE(request: NextRequest, context: { params: { id: number } }) {
     try {
         const TOKEN = request.cookies.get('token')?.value;
-        const data = await request.json();
-        const response = await fetch(API_URL + "/posts", {
-            method: "POST",
+        const params = await context.params;
+        const { id } = params;
+        const response = await fetch(API_URL + "/posts/" + id, {
+            method: "DELETE",
             headers: {
                 "Content-Type": "application/json",
                 "Authorization": "Bearer " + TOKEN
-            },
-            body: JSON.stringify(data)
+            }
         });
-
+        
         if (!response.ok) {
-            return NextResponse.json({ error: await response.json() }, { status: response.status });
+          return NextResponse.json({ error: await response.json() }, { status: response.status });
         }
+
         return NextResponse.json(await response.json(), { status: response.status });
     } catch (error) {
         throw error;
