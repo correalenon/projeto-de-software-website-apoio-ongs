d31c6ba2ee42717c69e5beea632ba4537cf52f35
Jamil Campeão <jamilcampeao@gmail.com>
2025-06-22 17:07:24 -0300

    chore: Implementado seção de projetos que o voluntário foi aceito no perfil do usuário

--- backend/src/routes/users.routes.js
+++ backend/src/routes/users.routes.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import { getMe, getUsers, getUserByID, postUser, deleteUserByID, putUser, PostEmailUser, PutPasswordUser, GetUsersWithoutONGs } from '../controllers/users.controller.js';
+import { getMe, getUsers, getUserByID, postUser, deleteUserByID, putUser, PostEmailUser, PutPasswordUser, GetUsersWithoutONGs, getUserAcceptedProjects } from '../controllers/users.controller.js';
 import { authenticateUserOrOng } from "../services/authentication.js";
 
 const router = Router();
 
 router.get("/not-associated-with-ongs", authenticateUserOrOng, GetUsersWithoutONGs);
+router.get("/accepted-projects", authenticateUserOrOng, getUserAcceptedProjects);
 router.get("/me", authenticateUserOrOng, getMe);
 router.get("/", authenticateUserOrOng, getUsers);
 router.get("/:id", authenticateUserOrOng, getUserByID);
